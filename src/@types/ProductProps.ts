c33e8b8d17b4403b7546a11d42e8544a3537cd88
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-14 10:44:33 -0300

    feat: types

---
+++ src/@types/ProductProps.ts
@@ -0,0 +1,11 @@
+import { CategoryProps } from "./CategoryProps";
+
+export interface ProductProps {
+    id?: string,
+    name: string,
+    price: number,
+    description?: string,
+    image: string,
+    idCategory?: string,
+    category: CategoryProps
+}
