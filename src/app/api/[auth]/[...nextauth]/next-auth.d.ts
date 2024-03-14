e1d68e10a16a963523a450070c6ef0048c133565
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-14 10:44:27 -0300

    feat: add NextAuth with Provider Credentials

---
+++ src/app/api/[auth]/[...nextauth]/next-auth.d.ts
@@ -0,0 +1,13 @@
+import { SessionProvider } from 'next-auth/react';
+import NextAuth from "next-auth";
+
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            id: string;
+            name: string;
+            email: string;
+        }
+    }
+}
