84e08d1252ea4c22a840580afa5dfc42428bdf6a
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-15 11:58:19 -0300

    feat: component Notifier and provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
 export default async function Home() {
+
+  const session = await getServerSession(authOptions)
+
+  console.log(session)
   return (
     <div>
-      teste
+      Home page
     </div>
   );
 }
