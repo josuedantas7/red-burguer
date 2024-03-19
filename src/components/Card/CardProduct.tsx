a7c44db11e95a1eb37ed7fc81de6230ee51b0cf9
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-19 03:51:47 -0300

    style: respons CardProduct

--- src/components/Card/CardProduct.tsx
+++ src/components/Card/CardProduct.tsx
@@ -14,12 +14,12 @@ export function CardProduct({product} : { product : CardProductProps }) {
   }
 
   return (
-    <div className='flex w-[612px] h-[144px] rounded-xl relative'>
+    <div className='flex w-[612px] h-[144px] max-[660px]:w-[500px] max-[550px]:w-[400px] rounded-xl relative'>
         <Image className='rounded-l-xl' src={product.image} width={144} height={144} alt='Burguer' />
         <div className='pl-4 relative flex flex-col gap-2'>
           <h2 className='font-semibold'>{product.name}</h2>
           <p>{product?.description}</p>
-          <h2 className='font-semibold absolute bottom-6'>{formatPrice(product.price)}</h2>
+          <h2 className='font-semibold absolute bottom-6 max-[550px]:bottom-2'>{formatPrice(product.price)}</h2>
         </div>
         <button>
           <MdOutlineAddShoppingCart className='absolute bg-black text-white rounded w-[60px] h-[30px] py-1 right-0 bottom-3' />
