8798fb3a800f713ad0eff26833d6181abd209297
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-29 16:29:34 -0300

    feat: ModalCartManagament

---
+++ src/components/Modal/ModalCartManagement.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import React, { ReactNode, useContext } from 'react'
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+import { CartContext } from '@/context/CartContext'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+
+export function ModalCartManagement({children} : { children : ReactNode }) {
+
+    const { cart } = useContext(CartContext)
+
+    console.log(cart)
+
+
+    function formatPrice(price: number) {
+        return price.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+    }
+
+  return (
+    <AlertDialog>
+        <AlertDialogTrigger className='w-full'>
+            {children}
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle className='text-center font-bold text-2xl'>Seu carrinho</AlertDialogTitle>
+                <div>
+                    {cart.length > 0 ? cart.map(item => {
+                        return (
+                            <div className='flex flex-col gap-1' key={item.id}>
+                                <span className='text-lg font-bold'>{item.name}</span>
+                                <span>Quantidade: {item.quantity}</span>
+                                <span>{item.quantity !== undefined ? formatPrice(item.price * item.quantity) : ''}</span>
+                            </div>
+                        )
+                    }) : <p className='text-center font-bold text-2xl'>Seu carrinho está vazio</p>}
+
+                    <div className='mt-5'>
+
+                        <form>
+                            <Label>Endereço</Label>
+                            <Input type='text' placeholder='Digite seu endereço completo...' />
+                        </form>
+                    </div>
+                </div>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+            <AlertDialogCancel>Fechar</AlertDialogCancel>
+            <AlertDialogAction>Finalizar Pedido</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+  )
+}
